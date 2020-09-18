d6f53d8294c47d1e78a22552388147f64d1aac9a
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-09-17 19:26:50 -0500

    base + adittionals configs

--- app/src/main/java/com/paul9834/spotifymvvm/MainActivity.kt
+++ app/src/main/java/com/paul9834/spotifymvvm/MainActivity.kt
@@ -2,10 +2,15 @@ package com.paul9834.spotifymvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+
     }
 }
